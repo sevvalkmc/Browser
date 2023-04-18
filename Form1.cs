87e6c3b7df233f9fa2905ca1dcfdc9d1e1f2e717
sevvalkmc <131072882+sevvalkmc@users.noreply.github.com>
2023-04-18 17:05:50 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -19,188 +19,188 @@ namespace Browser
         public Form1()
         {
             InitializeComponent();
-            Xpcom.Initialize("Firefox"); // Geckoyu başlatıcak firefox klasörünü belirttik. Projemizin içerisinde mevcut bu
+            Xpcom.Initialize("Firefox"); 
         }
         
-        string anasayfam = "https://www.google.com.tr"; // Anasayfa belirledik
-        GeckoWebBrowser gwb; // Geckowebbroserı gwb değişkenine atadık
+        string anasayfam = "https://www.google.com.tr"; 
+        GeckoWebBrowser gwb; 
 
-        void yeniSekme() //  Yeni sekme oluşturma fonksiyonumuz
+        void yeniSekme() 
         {
-            gwb = new GeckoWebBrowser(); // yeni bir tarayıcı oluşturduk
-            TabPage sekme = new TabPage(); // tabcontrol için yeni bir tabpage oluşturduk
-            tabControl1.TabPages.Add(sekme); // tabcontrole yeni sekmeyi ekledik
-            sekme.Controls.Add(gwb); // Sekmeye tarayıcımızı ekliyoruz
-            tabControl1.SelectTab(sekme); // Seçili tabı sekme olarak yapıyoruz yani her ctrl + t yapınca yeni sekmeyi gösteriyor
-            gwb.Navigate(anasayfam); // Anasayfaya git artık gwb de anasayfa görüntülenecek
-            gwb.Dock = DockStyle.Fill; // gwb yi sekmeye full sığdırdık
-            gwb.Navigated += Geckowb_Navigated; // gittiği anda fonksiyonu oluşturduk
+            gwb = new GeckoWebBrowser(); 
+            TabPage sekme = new TabPage(); 
+            tabControl1.TabPages.Add(sekme); 
+            sekme.Controls.Add(gwb); 
+            tabControl1.SelectTab(sekme); 
+            gwb.Navigate(anasayfam); 
+            gwb.Dock = DockStyle.Fill; 
+            gwb.Navigated += Geckowb_Navigated; 
         }
 
-        void sekmesil() // Sekme silme fonksiyonumuz
+        void sekmesil() 
         {
-            if (tabControl1.TabCount != 1) // Sekme sayısı 1 den farklıysa yani 2-3-4 se sekmeleri silebiliriz
+            if (tabControl1.TabCount != 1) 
             {
-                tabControl1.TabPages.Remove(tabControl1.SelectedTab); // seçili sekmeyi sil
-                tabControl1.SelectTab(tabControl1.TabPages.Count - 1); // sekme silindikten sonra son sekmeye geç
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+                tabControl1.SelectTab(tabControl1.TabPages.Count - 1);
             }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            tabControl1.Font = new Font("Georgia", 17); // tabcontrolün fontunu değiştirdik
-            yeniSekme(); // Yeni sekme oluştur
+            tabControl1.Font = new Font("Georgia", 17); 
+            yeniSekme(); 
         }
         string[] uzantilar = { "http://", "https://", "www.", "tr-tr.", ".com.tr", ".com", ".tr", ".org",
-            ".org.tr",".gov",".gov.tr",".net",".edu.tr",".edu",".info",".biz",".kim",".io","accounts.",".blogspot" }; // Web sitelerin uzantılarını diziye ekledik
+            ".org.tr",".gov",".gov.tr",".net",".edu.tr",".edu",".info",".biz",".kim",".io","accounts.",".blogspot" };
         private void Geckowb_Navigated(object sender, GeckoNavigatedEventArgs e) 
         {
-            gwb = sender as GeckoWebBrowser; // Hangi tarayıcıda olduğumuzu bulduk
-            TabPage sekme = gwb.Parent as TabPage; // sekmeyi tarayıcının atası olan tabpageye attık
-            ekle(); // ekle fonksiyonuyla gidilecek siteye ekledik
-            sekme.Text = gwb.Url.ToString(); // sekme.text e gidilen url atandı
-            comboBox1.Text = gwb.Url.ToString(); // sekme.text e gidilen url atandı
-            foreach (string item in uzantilar) // uzantılar dizisinin tek tek taradık
+            gwb = sender as GeckoWebBrowser; 
+            TabPage sekme = gwb.Parent as TabPage; 
+            ekle();
+            sekme.Text = gwb.Url.ToString();
+            comboBox1.Text = gwb.Url.ToString(); 
+            foreach (string item in uzantilar) 
             {
-                sekme.Text = sekme.Text.Replace(item,""); // uzantıları boşla değiştirdik.
+                sekme.Text = sekme.Text.Replace(item,"");
             }
-            int slashBul = sekme.Text.IndexOf('/'); // Elimizde slash kaldı 
-            if (slashBul != -1) sekme.Text = sekme.Text.Remove(slashBul); // ve ilk slashtan sonra sildik diğer kalanları yani artık elimizde google var www.google.com.tr/ falan yok
+            int slashBul = sekme.Text.IndexOf('/');  
+            if (slashBul != -1) sekme.Text = sekme.Text.Remove(slashBul);
             sekme.Text = sekme.Text.Substring(0, 1).ToUpper() + sekme.Text.Substring(1, sekme.Text.Length-1); 
-            // baş harfi büyük yaptık ve yanına baş harften sonraki karakterleri ekledik
+            
         }
         /// <summary>
         /// Geçmişe ve eski gidilen siteleri comboboxa ekle
         /// </summary>
         void ekle()
         {
-            listBox1.Items.Add(gwb.Url.ToString()); // Geçmiş listboxa ekleme yaptıktemizledik
-            toolStripStatusLabel_GidilenSite.Text = gwb.Url.ToString(); // asağıya gidilen siteyi gösteriyoruz.
-            comboBox1.Items.Add(gwb.Url.ToString()); // gidilen siteyi comboboxa  ekliyoruz
-            AutoCompleteStringCollection veri = new AutoCompleteStringCollection(); // comboboxa otomatik tamamlama özelliği veriyoruz.
-            foreach (string item in comboBox1.Items) // Comboboxtaki bütün elemanları 
+            listBox1.Items.Add(gwb.Url.ToString()); 
+            toolStripStatusLabel_GidilenSite.Text = gwb.Url.ToString(); 
+            comboBox1.Items.Add(gwb.Url.ToString());
+            AutoCompleteStringCollection veri = new AutoCompleteStringCollection(); 
+            foreach (string item in comboBox1.Items) 
             {
-                veri.Add(item); // otomatik tamamlama için veri ye ekliyoruz
+                veri.Add(item); 
             }
-            comboBox1.AutoCompleteCustomSource = veri; // otomatik tamamlamanın kaynağını veri yapıyoruz
+            comboBox1.AutoCompleteCustomSource = veri; 
         }
         int say = 0;
-        void git() // comboboxta entera basıldığında ve git butonuna basıldığında
+        void git() 
         {
-            foreach (var item in uzantilar) // eğer comboboxın içerisinde uzantılardan varsa
+            foreach (var item in uzantilar) 
             {
                 if (comboBox1.Text.Contains(item))
                 {
                     say++; // say ı bir arttır
                 }
             }
-            if (say >= 2) // eğer say 2 taneyse atıyorum wwww.google.com www. de var .com da var direk siteye git
+            if (say >= 2) 
             {
                 ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Navigate(comboBox1.Text);
                 say = 0;
             }
-            else if (say < 2) // eğer www. var .com yoksa veya tam tersi googlede ara
+            else if (say < 2) 
                 ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Navigate("www.google.com/search?q=" + comboBox1.Text);
 
         }
         private void comboBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter) // entera basıldığında git çalışssın
+            if (e.KeyCode == Keys.Enter) 
             {
                 git();
             }
         }
         private void pictureBox_refresh_Click(object sender, EventArgs e)
         {
-            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Refresh(); // Yenile butonu
+            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Refresh(); 
         }
 
         private void pictureBox_home_Click(object sender, EventArgs e)
         {
-            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Navigate(anasayfam); // Anasayfa yapıyorz
+            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Navigate(anasayfam); 
 
         }
 
         private void pictureBox_forward_Click(object sender, EventArgs e)
         {
-            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).GoForward(); // İleri git
+            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).GoForward(); 
         }
 
         private void pictureBox_back_Click(object sender, EventArgs e)
         {
-            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).GoBack(); // Geri git
+            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).GoBack(); 
         }
 
         private void pictureBox_go_Click(object sender, EventArgs e)
         {
-            git(); // git e basılınca
+            git(); 
         }
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Navigate(listBox1.SelectedItem.ToString()); // geçmişten bir nesne seçtiğimizde direk o siteye git
+            ((GeckoWebBrowser)(this.tabControl1.SelectedTab.Controls[0])).Navigate(listBox1.SelectedItem.ToString()); 
         }
 
         int gecmis_sayac = 0;
-        void gecmisAc() // ctrl + h ve geçmiş butonu için ortak kod
+        void gecmisAc() 
         {
             switch (gecmis_sayac)
             {
                 case 0:
-                    panel_gecmis.Visible = true; // sayac 0 sa gecmiş paneli görünsün yani tıklandıysa
+                    panel_gecmis.Visible = true; 
                     gecmis_sayac++; break;
                 case 1:
-                    panel_gecmis.Visible = false; // tekrar tıklandıysa gizlensin
+                    panel_gecmis.Visible = false; 
                     gecmis_sayac = 0; break;
                 default: break;
             }
         }
-        private void pictureBox_gecmis_Click(object sender, EventArgs e) /// geçmiş panelini aç kapa
+        private void pictureBox_gecmis_Click(object sender, EventArgs e) 
         {
             gecmisAc(); 
         }
 
         
-        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) // Bu ProcessCmdKey her türlü ihtimalde klavyeden tuşa basıldığında çalışır.
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) 
         {
-            if (keyData == (Keys.Control | Keys.T)) // CTRL + T ye  yeni sekme
+            if (keyData == (Keys.Control | Keys.T)) 
             {
                 yeniSekme();
                 return true;
             }
 
-            if (keyData == (Keys.Control | Keys.W)) // CTRL + W ye basılınca sekmeyi sil
+            if (keyData == (Keys.Control | Keys.W)) 
             {
                 sekmesil();
                 return true;
             }
-            if (keyData == (Keys.Control | Keys.H)) // CTRL + H ye basılınca geçmiş aç
+            if (keyData == (Keys.Control | Keys.H))
             {
                 gecmisAc();
                 return true;
             }
             return base.ProcessCmdKey(ref msg, keyData);
         }
-        private void Mouse_Hover(object sender, EventArgs e)// Butonların üstüne gelince efekt veriyoruz
+        private void Mouse_Hover(object sender, EventArgs e)
         {
-            PictureBox p = sender as PictureBox; // Hangi butona gelindiğini tespit et
-            p.BorderStyle = BorderStyle.Fixed3D; // butonun borderını Fixed3d yap
+            PictureBox p = sender as PictureBox; 
+            p.BorderStyle = BorderStyle.Fixed3D; 
         }
-        private void Mouse_Leave(object sender, EventArgs e) // Butonların üstünden gidince 
+        private void Mouse_Leave(object sender, EventArgs e)
         {
-            PictureBox p = sender as PictureBox; // Hangi butona gelindiğini tespit et
-            p.BorderStyle = BorderStyle.None; // butonun borderını kapat
+            PictureBox p = sender as PictureBox; 
+            p.BorderStyle = BorderStyle.None; 
         }
 
-        private void AraclarYeniSekmeClick(object sender, EventArgs e) // Araçlar kutusu yenisekme açma
+        private void AraclarYeniSekmeClick(object sender, EventArgs e) 
         {
             yeniSekme();
         }
 
-        private void tümSekmeleriKapatClick(object sender, EventArgs e) // Araçlar kutusu sekme kapatma
+        private void tümSekmeleriKapatClick(object sender, EventArgs e) 
         {
             tabControl1.TabPages.Clear();
             yeniSekme();
         }
-        private void anasayfaOlsunClick(object sender, EventArgs e) // Araçlar kutusu anasayfa ayarlama
+        private void anasayfaOlsunClick(object sender, EventArgs e) 
         {
             anasayfam = gwb.Url.ToString();
         }
